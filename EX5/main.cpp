37e5665c1056984923142ea2913ef9e5d2e1636b
John20050201 <151489373+John20050201@users.noreply.github.com>
2024-05-23 13:50:19 +0800

    Create main.cpp

---
+++ EX5/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include<string>
+#include<fstream>
+
+using namespace std;
+
+class myString
+{
+    private:
+    string m_str;
+    public:
+    myString(string s){
+        m_str=s;
+    }
+};
+
+class ReadClass
+{
+    private:
+    int amount=0;
+    string filename="main.cpp";
+    
+    public:
+    ReadClass(){}
+    void showClass(){
+    ifstream in;
+    string line;
+    in.open(filename);
+    if(in.fail()){
+        cout<<"Error open a file"<<endl;
+        in.close();
+    }
+    while(getline(in,line)){
+        if(line[0]=='c'){
+            amount+=1;
+        }
+    }
+    cout<<amount<<" class in "<<filename<<endl;
+    in.close();
+    in.open(filename);
+    while(getline(in,line)){
+        if(line[0]=='c'){
+            cout<<line<<endl;
+        }
+    }
+    }
+};
+
+int main()
+{
+    ReadClass rfile;
+    rfile.showClass();
+    return 0;
+}
